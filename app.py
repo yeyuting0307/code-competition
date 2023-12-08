ab553a333982be06a43dc2d4bf1df277b847e57c
Yu-Ting Ye <mike@Yu-TingdeMacBook-Pro.local>
2023-12-09 00:18:21 +0800

    index.py add input and output block

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, template_folder = "templates")
 app.config['SECRET_KEY'] = 'secret!'
@@ -11,12 +11,20 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
+    # TODO: create notebook
     print('Client connected 😀')
-
 
 @socketio.on('disconnect')
 def handle_disconnect():
+    # TODO: delete notebook
     print('Client disconnected 🥲')
+
+@socketio.on('message')
+def handle_message(message):
+    # TODO: update notebook
+    print('received message: ' + message)
+    response_message = message.upper()
+    emit('response_message', {'data': response_message})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
