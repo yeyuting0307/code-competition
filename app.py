b6669c147780a95fd7a500510e6f3f85b2351ec8
Yu-Ting Ye <mike@Yu-TingdeMacBook-Pro.local>
2023-12-11 01:36:51 +0800

    socketio connect with jupyter nb create

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from services.jupyter_crud import Jupyter
+import dotenv
+from hashlib import sha1
+
+dotenv.load_dotenv()
+JUPYTER_TOKEN = os.environ.get("JUPYTER_NOTEBOOK_TOKEN", "")
 
 app = Flask(__name__, template_folder = "templates")
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+jupyter = Jupyter(jupyter_token=JUPYTER_TOKEN)
+cache = {}
 
 @app.route('/')
 def index():
@@ -11,8 +20,19 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
-    # TODO: create notebook
     print('Client connected 😀')
+    cookie = list(request.cookies.to_dict().values())[0]
+
+    if cookie not in cache:
+        nb_name = f"{sha1(cookie.encode()).hexdigest()}.ipynb"
+        cache[cookie] = nb_name 
+        if os.path.exists(os.path.join("notebooks", nb_name)):
+            print(f"{nb_name} existed!")
+        else:
+            jupyter.create_notebook(new_nb_name=nb_name)
+    cookie_nb = jupyter.read_notebook(nb_name=cache[cookie])
+    print(cookie_nb)
+    
 
 @socketio.on('disconnect')
 def handle_disconnect():
